f32079f8a366c2a424ba7da60e967ecdb2bc91c2
yxx790 <yxx790@gmail.com>
2023-07-13 22:58:30 +0500

    dating first commit

---
+++ dating/script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const allHeadBtns = document.querySelectorAll('nav > div');
+const allPages = document.querySelectorAll('main > div');
+allHeadBtns.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+        allPages.forEach(page => { page.setAttribute("hidden", true) });
+        allPages[index].removeAttribute("hidden");
+        allHeadBtns.forEach(b => { b.classList.remove("activeBtn") });
+        btn.classList.add("activeBtn");
+    })
+})
+
+for (let age = 18; age < 100; age++) {
+    document.querySelector("#userAge").innerHTML +=
+        `<option value="${age}">${age}</option>`
+}
+
+
