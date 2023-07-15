085a508280dfcf178715be0acbb4a8aaf810dc1f
yxx790 <yxx790@gmail.com>
2023-07-16 01:35:38 +0500

    4dvh

--- JS/blackjack.js
+++ JS/blackjack.js
@@ -5,7 +5,7 @@ if (tg?.initDataUnsafe?.user?.id) {
     document.querySelector("header").remove();
     // const br = document.createElement("br");
     // document.querySelector("#BJ").prepend(br);
-    document.querySelector("h3").style.fontSize = "3dvh";
+    document.querySelector("h3").style.fontSize = "4dvh";
 }
 if (tg?.initDataUnsafe?.user?.username) {
     document.querySelector("#yourName").textContent =
