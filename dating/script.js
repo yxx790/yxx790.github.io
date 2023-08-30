f417b67c5d9acc9d5ff50eac517a86672d351da6
yxx790rem <yxx790@gmail.com>
2023-08-30 17:58:15 +0500

    js

--- dating/script.js
+++ dating/script.js
@@ -1,26 +1,23 @@
 'use strict';
-const tg = window?.Telegram?.WebApp; //Initializes the TELEGRAM BOT and
-//Gets the user's Telegram ID from the Telegram API
+const tg = window?.Telegram?.WebApp;
 // screen.orientation.lock(ORIENTATION);
-tg?.expand(); //Expands the app on the users' phone to 100% height
+tg?.expand();
 
 const consol = document.querySelector("#consol");
 function print(data){consol.innerHTML += data +'<br>'};
 
-print('colorScheme = ' + tg?.colorScheme)
+//print('colorScheme = ' + tg?.colorScheme)
 print('initDataUnsafe.user.id = ' + tg?.initDataUnsafe?.user?.id)
 print('initData = ' + tg?.initData)
-
-document.querySelector('#profile').addEventListener('click', () => {
-    consol.toggleAttribute('hidden')});
-
-
 if (tg?.colorScheme == "dark"){
     document.documentElement.style.setProperty('--background', '#252550');
     document.documentElement.style.setProperty('--text', '#e0e0e0');
     print('Dark theme enabled');
     // document.querySelector(":root");
 }
+
+document.querySelector('#profile').addEventListener('click', () => {
+    consol.toggleAttribute('hidden')});
 
 const allHeadBtns = document.querySelectorAll('nav > div');
 const allPages = document.querySelectorAll('main > div');
@@ -30,7 +27,7 @@ allHeadBtns.forEach((btn, index) => {
         allPages[index].removeAttribute("hidden");
         allHeadBtns.forEach(b => { b.classList.remove("activeBtn") });
         btn.classList.add("activeBtn");
-        print(`${btn.id} activated`);
+        // print(`${btn.id} activated`);
     })
 })
 
