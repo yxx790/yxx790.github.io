57d3d072859f77db377cc14ac2906802f8fd503d
yxx790rem <yxx790@gmail.com>
2023-09-08 14:13:56 +0500

    bugs fixed

--- JS/header.js
+++ JS/header.js
@@ -6,9 +6,9 @@ let header =`
     <button class="headmenu">&#9776;</button>
     <div class="headlist">
         <button><a href="index.html">Home</a></button>
-       <button>Hashcat</button>
-       <button>El Catino</button>
-       <button><a href="blackjack.html">Black Jack</a></button>
+        <button>Hashcat</button>
+        <button>El Catino</button>
+        <button><a href="blackjack.html">Black Jack</a></button>
    </div>
 </div>
 <button>Profile</button>
